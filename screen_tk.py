bc057fcf83a9292abb7c1a548bc3f9f8d2cd19f2
張之耀(Chih-Yao, Chang) RTK <m900054@gmail.com>
2021-09-27 16:57:30 +0800

    add new feature to screen

--- screen_tk.py
+++ screen_tk.py
@@ -1,78 +1,160 @@
 import pyautogui
 import tkinter as tk
+import math
 
 def move(event):
     global x, y ,xstart,ystart
-    new_x = (event.x-x)+canvas.winfo_x()
-    new_y = (event.y-y)+canvas.winfo_y()
-    s = "300x200+" + str(new_x)+"+" + str(new_y)    
-    canvas.place(x = new_x - xstart,y = new_y -ystart)   
-    print("s = ", s)
+    # new_x = (event.x-x)+canvas.winfo_x()
+    # new_y = (event.y-y)+canvas.winfo_y()
+    # s = "300x200+" + str(new_x)+"+" + str(new_y)    
+    # canvas.place(x = new_x - xstart,y = new_y -ystart)
+    canvas.place(x = event.x, y = event.y)
+    # print("s = ", s)
     print(root.winfo_x(), root.winfo_y())
     print(event.x, event.y)
 
 def button_1(event):
-    global x, y ,xstart,ystart
+    global x, y ,xstart, ystart, place_x1, place_y1, block_w, block_h
     global rec
-    x, y = event.x, event.y
-    xstart,ystart = event.x, event.y
-    print("event.x, event.y = ", event.x, event.y)
-    xstart,ystart = event.x, event.y  
-    cv.configure(height=1)
-    cv.configure(width=1)
-    cv.config(highlightthickness=0) # 無邊框
-    cv.place(x=event.x, y=event.y)
-    rec = cv.create_rectangle(0,0,0,0,outline='red',width=8,dash=(4, 4))
+    x = root.winfo_pointerx() - root.winfo_rootx()
+    y = root.winfo_pointery() - root.winfo_rooty()
+    if x >= cfg_x1 and x < cfg_x2 and y >= cfg_y1 and y < cfg_y2:
+        xstart, ystart = x, y
+        #print("button 1 press : x, button 1 press : y = ", x, y)
+        new_w = x - cfg_x1 + 1
+        new_h = y - cfg_y1 + 1
+        place_x1 = cfg_x1 + ((math.ceil(new_w/float(txt_w)) - 1) * txt_w)
+        place_y1 = cfg_y1 + ((math.ceil(new_h/float(txt_h)) - 1) * txt_h)
+        block_w = txt_w
+        block_h = txt_h
+        motion_cv.configure(width=block_w)
+        motion_cv.configure(height=block_h)
+        motion_cv.place(x=place_x1, y=place_y1)
+    else:
+        motion_cv.place_forget()
+        root.attributes("-alpha", 0)
+        print('screen fail')
+        sys_out(None)
 
 def b1_Motion(event):
-    global x, y,xstart,ystart
-    x, y = event.x, event.y
-    print("event.x, event.y = ", event.x, event.y)
-    cv.configure(height = event.y - ystart)
-    cv.configure(width = event.x - xstart)
-    cv.coords(rec,0,0,event.x-xstart,event.y-ystart)
+    global x, y, xstart, ystart, canvas_text_handle, canvas_text, place_x1, place_y1, block_w, block_h
+    x = root.winfo_pointerx() - root.winfo_rootx()
+    y = root.winfo_pointery() - root.winfo_rooty()
+    if x >= cfg_x1 and x < cfg_x2 and y >= cfg_y1 and y < cfg_y2:
+        #print("event.x, event.y = ", x, y)
+        canvas.place(x = x, y = y)
+        canvas_text = 'press ESC to exit\n' + str(x - xstart) + ', ' + str(y - ystart)
+        canvas.itemconfig(canvas_text_handle, text=canvas_text)
+
+        new_w = x - place_x1 + 1
+        new_h = y - place_y1 + 1
+        block_w = math.ceil(new_w/float(txt_w)) * txt_w
+        block_h = math.ceil(new_h/float(txt_h)) * txt_h
+        if block_w >= cfg_x2-cfg_x1:
+            block_w = cfg_x2-cfg_x1
+        if block_h >= cfg_y2-cfg_y1:
+            block_h = cfg_y2-cfg_y1
+        motion_cv.configure(width=block_w)
+        motion_cv.configure(height=block_h)
 
 def buttonRelease_1(event):
-    global xend,yend
-    xend, yend = event.x, event.y
+    global xend, yend, place_x1, place_y1, block_w, block_h
+    xend, yend = root.winfo_pointerx() - root.winfo_rootx(), root.winfo_pointery() - root.winfo_rooty()
+    if xend >= cfg_x1 and xend < cfg_x2 and yend >= cfg_y1 and yend < cfg_y2:
+        if xend <= place_x1 or yend <= place_y1:
+            motion_cv.place_forget()
+            root.attributes("-alpha", 0)
+            print('screen fail')
+            sys_out(None)
+        else:
+            motion_cv.place_forget()
+            root.attributes("-alpha", 0)
+            img = pyautogui.screenshot(region=[place_x1, place_y1, block_w, block_h]) # x,y,w,h
+            img.save('screenshot.png')
+            sys_out(None)
+    else:
+        motion_cv.place_forget()
+        root.attributes("-alpha", 0)
+        print('screen fail')
+        sys_out(None)
 
 def button_3(event):
     global xstart,ystart,xend,yend
-    cv.delete(rec)
-    cv.place_forget()
+    root.attributes("-alpha", 0)
     img = pyautogui.screenshot(region=[xstart,ystart,xend-xstart,yend-ystart]) # x,y,w,h
     img.save('screenshot.png')
     sys_out(None)
 
+def my_motion_test(event=None):
+    global place_x1, place_x2, place_y1, place_y2
+    x = root.winfo_pointerx() - root.winfo_rootx()
+    y = root.winfo_pointery() - root.winfo_rooty()
+    if x >= cfg_x1 and x < cfg_x2 and y >= cfg_y1 and y < cfg_y2:
+        new_w = x - cfg_x1 + 1
+        new_h = y - cfg_y1 + 1
+        place_x1 = cfg_x1 + ((math.ceil(new_w/float(txt_w)) - 1) * txt_w)
+        place_y1 = cfg_y1 + ((math.ceil(new_h/float(txt_h)) - 1) * txt_h)
+        #print(x, y, place_x1, place_y1)
+        motion_cv.configure(width=txt_w)
+        motion_cv.configure(height=txt_h)
+        motion_cv.place(x=place_x1, y=place_y1)
+    else:
+        motion_cv.configure(width=1)
+        motion_cv.configure(height=1)
+    
+
 def sys_out(even):
     root.destroy()
 
+
 root = tk.Tk()
 root.overrideredirect(True)         # 隱藏視窗的標題列
-root.attributes("-alpha", 0.1)      # 視窗透明度10%
+root.attributes("-alpha", 0.3)      # 視窗透明度30%
 root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
-root.configure(bg="black")
+#root.configure(bg="black")
 
-# 再建立1個Canvas用於圈選
-cv = tk.Canvas(root)
 x, y = 0, 0
 xstart,ystart = 0 ,0
 xend,yend = 0, 0
 rec = ''
 
 canvas = tk.Canvas(root)
-canvas.configure(width=300)
-canvas.configure(height=100)
+canvas.configure(width=120)
+canvas.configure(height=50)
 canvas.configure(bg="yellow")
 canvas.configure(highlightthickness=0)  # 高亮厚度
-canvas.place(x=(root.winfo_screenwidth()-500),y=(root.winfo_screenheight()-300))
-canvas.create_text(150, 50,font='Arial -20 bold',text='ESC退出，假裝工具條')
+canvas.place(x=(root.winfo_screenwidth()-500), y=50)
+canvas_text = 'press ESC to exit'
+canvas_text_handle = canvas.create_text(60, 25,font='Arial -12 bold',text=canvas_text)
+
+
+txt_w = 9
+txt_h = 18
+cfg_x1 = 1
+cfg_y1 = 144
+cfg_x2 = 1900
+cfg_y2 = 972 + txt_h*2
+place_x1 = 0
+place_x2 = 0
+place_y1 = 0
+place_y2 = 0
+block_w = 0
+block_h = 0
+motion_cv = tk.Canvas(root)
+# motion_cv.configure(width=cfg_x2-cfg_x1)
+# motion_cv.configure(height=cfg_y2-cfg_y1)
+motion_cv.configure(width=txt_w)
+motion_cv.configure(height=txt_h)
+motion_cv.configure(bg="red")
+motion_cv.config(highlightthickness=0) # 無邊框
+motion_cv.place(x=cfg_x1, y=cfg_y1)
 
 # 繫結事件
-canvas.bind("<B1-Motion>", move)   # 滑鼠左鍵移動->顯示當前遊標位置
+#root.bind("<B1-Motion>", move)   # 滑鼠左鍵移動->顯示當前遊標位置
 root.bind('<Escape>',sys_out)      # 鍵盤Esc鍵->退出
 root.bind("<Button-1>", button_1)  # 滑鼠左鍵點選->顯示子視窗 
 root.bind("<B1-Motion>", b1_Motion)# 滑鼠左鍵移動->改變子視窗大小
 root.bind("<ButtonRelease-1>", buttonRelease_1) # 滑鼠左鍵釋放->記錄最後遊標的位置
-root.bind("<Button-3>",button_3)   #滑鼠右鍵點選->截圖並儲存圖片
+#root.bind("<Button-3>",button_3)   #滑鼠右鍵點選->截圖並儲存圖片
+root.bind("<Motion>", my_motion_test)
 root.mainloop()
